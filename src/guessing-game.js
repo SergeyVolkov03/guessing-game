8e2cf69e307e26e6d34d7e4ff775151795901e27
Sergey Volkov <serzhvolkov03@gmail.com>
2023-02-25 16:09:19 +0300

    feat: imolement GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
     constructor() {}
-
+  
     setRange(min, max) {
-
+      this.maxValue = max;
+      this.minValue = min;
     }
-
+  
     guess() {
-
+      this.guessValue = Math.round((this.maxValue + this.minValue) / 2);
+      return this.guessValue;
     }
-
+  
     lower() {
-
+      this.maxValue = this.guessValue;
     }
-
+  
     greater() {
-
+      this.minValue = this.guessValue;
     }
-}
+  }
 
 module.exports = GuessingGame;
